c54093134470b3aeeb37bb5e85d120f43638e962
maxo <max.orr@mail.com>
2025-07-15 11:23:11 -0400

    build: moving weatherflow grafana logic

--- grafana/build.sh
+++ grafana/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-docker build --no-cache . -t maxo99/grafana:latest
+# Read version from parent directory
+VERSION=$(grep -oP '(?<=version=).*' ../version)
+
+docker build --no-cache -t maxo99/grafana:${VERSION} -t maxo99/grafana:latest  .
 # docker push maxo99/grafana:latest
 
