b9c8f02acd1e8ddc8d96d1fa69dd5bb4c12984b3
maxo <max.orr@mail.com>
2025-07-16 19:13:55 -0400

    feat(grafana): initial grafana dashboards import and provisioning for weathercollector data

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+# WeatherFlow Collector
+cp .env weatherflow-collector/.env
+cd weatherflow-collector
+bash build.sh
+cd ..
+rm -f weatherflow-collector/.env
+
+
+# # Grafana
+# cp .env grafana/.env
+# cd grafana
+# bash build.sh
+# cd ..
+# rm -f grafana/.env
+
--- grafana/build.sh
+++ grafana/build.sh
@@ -3,6 +3,27 @@
 # Read version from parent directory
 VERSION=$(grep -oP '(?<=version=).*' ../version)
 
+
+GRAFANA_DIR=../docker_data/grafana_data
+
+## Copy Provisioning DataSource
+mkdir -p ${GRAFANA_DIR}/datasources
+cp -r datasources ${GRAFANA_DIR}/datasources
+
+## Copy Provisioning Dashboards
+mkdir -p ${GRAFANA_DIR}/dashboards
+cp -r dashboards ${GRAFANA_DIR}/dashboards
+
+# # Copy Dashboards from weatherflow collector into Grafana Data
+# mkdir -p grafana-data/dashboards/weatherflow_collector
+# mkdir -p grafana-data/dashboards/weatherflow_collector2
+
+
+# # Copy dashboard files from your source
+# cp dashboards/weatherflow-collector/*.json grafana-data/dashboards/weatherflow_collector/
+# cp dashboards/weatherflow-collector2/*.json grafana-data/dashboards/weatherflow_collector2/
+
+
 docker build --no-cache -t maxo99/grafana:${VERSION} -t maxo99/grafana:latest  .
 # docker push maxo99/grafana:latest
 
